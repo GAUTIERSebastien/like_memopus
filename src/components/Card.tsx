935b3358c3091f0e1c69b8d355718cf269696117
Sébastien GAUTIER <sgautier034@gmail.com>
2023-10-03 15:59:32 +0200

    Update => card ok

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,9 +1,41 @@
-const Card = (card: any) => {
+import React, { useState } from 'react';
+import JsonCards from '../services/JsonCards';
+
+const Card = (props: any) => {
+    const [isEditing, setIsEditing] = useState(false);
+    const [question, setQuestion] = useState(props.question);
+    const [answer, setAnswer] = useState(props.answer);
+
+    const saveChanges = () => {
+        const updatedCard = { ...props, question, answer };
+        JsonCards.updateCard(updatedCard).then(() => {
+            setIsEditing(false);
+        });
+    };
+
     return (
-        <div className="card">
+        <div className="card mb-3">
             <div className="card-body">
-                <h6 className="card-title">Question: {card.question}</h6>
-                <p className="card-text">Réponse: {card.answer}</p>
+                {isEditing ? (
+                    <div>
+                        <div className="mb-3">
+                            <label htmlFor="questionInput" className="form-label">Question</label>
+                            <input type="text" className="form-control" id="questionInput" value={question} onChange={(e) => setQuestion(e.target.value)} />
+                        </div>
+                        <div className="mb-3">
+                            <label htmlFor="answerInput" className="form-label">Réponse</label>
+                            <input type="text" className="form-control" id="answerInput" value={answer} onChange={(e) => setAnswer(e.target.value)} />
+                        </div>
+                        <button className="btn btn-success" onClick={saveChanges}>Enregistrer</button>
+                        <button className="btn btn-secondary ms-2" onClick={() => setIsEditing(false)}>Annuler</button>
+                    </div>
+                ) : (
+                    <>
+                        <h6 className="card-title">Question: {question}</h6>
+                        <p className="card-text">Réponse: {answer}</p>
+                        <button className="btn btn-primary" onClick={() => setIsEditing(true)}>Modifier</button>
+                    </>
+                )}
             </div>
         </div>
     );
--- src/services/JsonCards.tsx
+++ src/services/JsonCards.tsx
@@ -14,4 +14,19 @@ export default class JsonCards {
           console.error(`Erreur attrapée dans loadCards : ` + error);
         });
     }
+
+    static async updateCard(card: any) {
+      return fetch(`${JsonCards.url}/${card.id}`, {
+          method: 'PUT',
+          headers: {
+              'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(card),
+      }).then((response) => {
+          return response.json();
+      }).catch((error) => {
+          console.error(`Erreur lors de la mise à jour de la card: ` + error);
+      });
+  }
+  
 }
